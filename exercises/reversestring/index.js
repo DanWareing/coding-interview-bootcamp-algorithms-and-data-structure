43785b91b84d7140a722b2623762ef8f95c51a63
DanWareing-N26 <daniel.wareing@n26.com>
2019-02-01 12:50:01 +0100

    [1] Reverse String

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,21 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+// function reverse(str) {
+//   const arr = str.split('');
+//   return arr.reverse().join('');
+// }
+
+// function reverse(str) {
+//   let reversed = '';
+//   for (let char of str) {
+//     reversed = char + reversed;
+//   }
+//   return reversed;
+// }
+
+function reverse(str) {
+  return str.split('').reduce((rev, char) => char + rev, '');
+}
 
 module.exports = reverse;
