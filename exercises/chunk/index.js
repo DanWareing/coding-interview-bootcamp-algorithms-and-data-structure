806d3f0cf839523abd867b160d7720691fa9add5
DanWareing-N26 <daniel.wareing@n26.com>
2019-02-19 14:47:59 +0100

    [6] Chunk

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,14 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+  let topArray = [];
+  while (array.length > 0) {
+    let endIndexInt = Math.min(size, array.length);
+    topArray.push(array.slice(0, endIndexInt));
+    array.splice(0, endIndexInt);
+  }
+  return topArray;
+}
 
 module.exports = chunk;
