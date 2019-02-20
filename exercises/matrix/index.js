1dcf9c71f6b591f5f3ac048c459011f5511a2f6e
DanWareing-N26 <daniel.wareing@n26.com>
2019-02-20 15:36:59 +0100

    [11] Matrix

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,6 +15,59 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(n) {
+  let arr = [];
+  for (var i=1; i<=n; i++) {
+    arr.push(new Array(n));
+  }
+
+  for (var j=1; j<=n*n; j++) {
+    let offset=0;
+    while (offset<arr.length && !addNumber(j, arr, offset)) {
+      offset++;
+    }
+  }
+
+  return arr;
+}
+
+function addNumber(value, array, offset) {
+  let x=0+offset;
+  let y=0+offset;
+  let width = array.length-(1+offset);
+
+  // Work right
+  for (x;x<width;x++) {
+    if (y<=width && x<=width && !array[y][x]) {
+      array[y][x] = value;
+      return array;
+    }
+  }
+
+  // Work down
+  for (y;y<width;y++) {
+    if (y<=width && x<=width && !array[y][x]) {
+      array[y][x] = value;
+      return array;
+    }
+  }
+
+  // Work left
+  for (x;x>0;x--) {
+    if (y<=width && x<=width && !array[y][x]) {
+      array[y][x] = value;
+      return array;
+    }
+  }
+
+  // Work up
+  for (y;y>0;y--) {
+    if (y<=width && x<=width && !array[y][x]) {
+      array[y][x] = value;
+      return array;
+    }
+  }
+
+}
 
 module.exports = matrix;
